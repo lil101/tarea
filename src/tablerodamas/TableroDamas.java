22e546f713b33316bb0621b897297a41f05a8436
ISTLOJAV <ISTLOJAV@ISTLOJA05>
2019-06-20 10:02:20 +0200

    Sincronización IST Loja

---
+++ src/tablerodamas/TableroDamas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablerodamas;
+
+/**
+ *
+ * @author JOSELIN CARPIO
+ */
+public class TableroDamas {
+
+//El metodo main inicia la ejecucion de la aplicacion en java
+    public static void main(String[] args) {
+        {
+            System.out.println("********");
+            System.out.println(" ********");
+            System.out.println("********");
+            System.out.println(" ********");
+            System.out.println("********");
+            System.out.println(" ********");
+            System.out.println("********");
+            
+            System.exit(0);
+               
+    } //fin del metodo main
+      
+       
+    }
+    
+}
