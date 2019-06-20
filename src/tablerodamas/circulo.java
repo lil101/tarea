51e940f76a2df5d46bc3b3537818de1769512ae6
ISTLOJAV <ISTLOJAV@ISTLOJA05>
2019-06-20 10:08:42 +0200

    Sincronización IST Loja

---
+++ src/tablerodamas/circulo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablerodamas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JOSELIN CARPIO
+ */
+public class circulo {
+    //método clase o método estático
+    public static void main(String[] args) {
+        int radio; //almacena el radio del circulo
+        
+        //instancia un objeto de tipo "Scanner"
+        Scanner entrada= new Scanner (System.in);
+        
+        //solicitud y obtencion del radio del circulo
+        System.out.println("Digite el radio");
+        radio=entrada.nextInt();
+        
+        //visualizacion de resultados
+        //diametro
+        System.out.printf("\nEl diametro del circulo es: %d.",(2*radio));
+        
+        //circunferencia
+        System.out.printf("\nLa circunferencia del circulo es: %f.",(2*Math.PI*radio));
+        
+        //area
+        System.out.printf("\nEl area del circulo es: %f.\n",(Math.PI*radio*radio));
+        
+        //salida normal
+        System.exit(0);
+    } //fin del metodo main
+    
+}
---
+++ src/tablerodamas/separacion_de_digitos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablerodamas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JOSELIN CARPIO
+ */
+public class separacion_de_digitos {
+
+    public static void main(String[] args) {
+        //abre metodo main
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        System.out.println("\nIntroduzca un numero de cinco cifras y sera impresa con espacios");
+        numero = entrada.nextInt();
+        System.out.printf("%d ",   numero/10000);
+        System.out.printf("%d ", (numero%10000)/1000);
+        System.out.printf("%d ", ((numero%10000)%1000)/100);
+        System.out.printf("%d ", (((numero%10000)%1000)%100)/10);
+        System.out.printf("%d\n ", ((((numero%10000)%1000)%100)%10));
+
+    } //cierra main
+
+}
